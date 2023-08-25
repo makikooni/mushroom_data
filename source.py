4216d3caf3e7c6c86f420532ff0a2a135b9224e3
makikooni <w.e.falinska@gmail.com>
2023-08-25 20:35:03 +0100

    create_functions, find_modes

--- source.py
+++ source.py
@@ -5,16 +5,34 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("mushroom_data.csv")
+print(df.head())
 
-output_directory = "/Users/makikooni/Github/mushroom_data/Figures"
+def plot_graphs(df):
+    output_directory = "/Users/makikooni/Github/mushroom_data/Figures"
+    columns = df.columns.tolist()
+    for column in columns:
+        sns.countplot(data=df, x=column)
+        output_filename = os.path.join(output_directory, f"{column}_countplot.png")
+        plt.savefig(output_filename)
+        #plt.show()
+        plt.close()
 
-columns = df.columns.tolist()
-for column in columns:
-    sns.countplot(data=df, x=column)
-    output_filename = os.path.join(output_directory, f"{column}_countplot.png")
-    plt.savefig(output_filename)
+def find_modes(df,column_choice):
+    total_rows = len(df)
+    modes = df.mode()
+
+    mode_values = {}  # Create a dictionary to store mode values
+
+    for column in df.columns:
+        mode_value = modes.loc[0, column]
+        mode_count = (df[column] == mode_value).sum()
+        mode_percentage = (mode_count / total_rows) * 100
+        mode_values[column] = (mode_value, mode_percentage)
+        
+        if column == column_choice:
+            print(f"Mode of the column {column} is: {mode_value}, Percentage: {mode_percentage:.2f}%")
     
-    plt.close()
+    return mode_values
 
-
-#  python3 /Users/makikooni/Github/mushroom_data/source.py
+find_modes(df,'Habitat')
+#python3 /Users/makikooni/Github/mushroom_data/source.py
